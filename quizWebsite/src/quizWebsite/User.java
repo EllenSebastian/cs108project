d7961ee5bed0600b9bc470de2385df35f543dca3
huanghai <yc646@126.com>
2014-02-24 10:59:07 -0800

    added user related functionality by hai

--- quizWebsite/src/quizWebsite/User.java
+++ quizWebsite/src/quizWebsite/User.java
@@ -1,16 +1,48 @@
 package quizWebsite;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.List;
 
 public class User {
-	String name;
-	String hashPass;
-	ArrayList<String> achievements;
-	Activity activity;
-	ArrayList<String> friends;
-	ArrayList<Message> messages;
-	public User(){
+	
+	private String name;
+	public int id;
+	private String password;
+	private boolean isAdmin;
 		
+	// need to implement the connection to Database
+	private static Connection db = myDBinfo.getConnection();
+    
+	// id refer to the key of this particular user in database
+	public User(int id,String name,String password,boolean isAdmin){
+		this.name = name;
+		this.password = password;
+		this.id = id;
+		this.isAdmin = isAdmin;
+	}
+	
+	// retrieve the user information from db and create a user object
+	public static User getUser(int id){
+		ResultSet r;
+		try {
+			r = db.prepareStatement("SELECT * FROM user WHERE user_id = " + id).executeQuery();
+			if(!r.next()) return null;
+			User user = new User(r.getInt("user_id"),
+					          r.getString("name"), 					           	
+					          r.getString("password"),
+							  r.getInt("isAdmin") == 1 ? true : false
+							 );
+			return user;
+		} catch (SQLException e) {
+			return null;
+		}
 	}
 	
 	public Achievements getAchievements(){
--- quizWebsite/src/quizWebsite/UserManager.java
+++ quizWebsite/src/quizWebsite/UserManager.java
@@ -1,12 +1,150 @@
 package quizWebsite;
 
+import java.security.MessageDigest;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
+
 public class UserManager {
-	ArrayList<User> list;
 	
-	public UserManager(){
-		list = new ArrayList<User>();
+	private static Connection db = myDBinfo.getConnection();
+	
+	private static String hashPassword(String text) {
+		byte[] hash = new byte[40];
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			md.update(text.getBytes("utf-8"), 0, text.length());
+			hash = md.digest();
+		} catch (Exception ignored) { }
+		return hexToString(hash);
 	}
 	
+	private static String hexToString(byte[] bytes) {
+		StringBuffer buff = new StringBuffer();
+		for (int i=0; i<bytes.length; i++) {
+			int val = bytes[i];
+			val = val & 0xff;  // remove higher bits, sign
+			if (val<16) buff.append('0'); // leading 0
+			buff.append(Integer.toString(val, 16));
+		}
+		return buff.toString();
+	}
+	
+	
+	// in login, check whether the user exists and return the user or null(user not existed)
+	public static User checkUser(String name,String password){	
+		password = hashPassword(password);
+		
+		try {			
+			PreparedStatement p = db.prepareStatement("SELECT * FROM user WHERE name = ?");
+			p.setString(1, name);
+			
+			ResultSet result = p.executeQuery();
+			if(!result.next()) return null;
+			if(!result.getString("password").equals(password)) return null;
+			
+			return User.getUser(result.getInt("user_id"));
+		} catch (SQLException e) {
+			return null;
+		}
+	}
+	
+	// in sign up, insert a new user into database and return it
+	public static User addUser(String name,String password,boolean isAdmin){
+		password = hashPassword(password);
+		
+		try {
+			PreparedStatement p = db.prepareStatement("INSERT IGNORE INTO user (name, password, isAdmin) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+			p.setString(1, name);
+			p.setString(2, password);
+			p.setInt(3, isAdmin ? 1 : 0);
+			
+			int changed = p.executeUpdate();
+			if(changed == 0) return null;
+			
+			// return the latest inserted user
+			ResultSet s = p.getGeneratedKeys();
+			s.next();
+			return User.getUser(s.getInt(1));
+		} catch (SQLException e) {
+			return null;
+		}
+	}
+	
+	public static int numAllUsers() {
+		try {
+			ResultSet result = db.prepareStatement("SELECT COUNT(*) FROM user").executeQuery();	
+			int numUsers = 0;			
+			if (result.next()) {
+				numUsers += result.getInt(1);
+			}
+			return numUsers;
+		} catch (SQLException e) {
+			return 0;
+		}
+	}
+	
+	// make a user admin 
+	public static void promoteUser(int id) {
+		try {
+			db.prepareStatement("UPDATE user SET isAdmin = 1 WHERE user_id = " + id).executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// make a user non-admin	
+	public static void demoteUser(int id){
+		try {
+			db.prepareStatement("UPDATE user SET isAdmin = 0 WHERE user_id = " + id).executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static ArrayList<User> getAdmins() {
+		try {
+			ArrayList<User> results = new ArrayList<User>();
+			PreparedStatement p = db.prepareStatement("SELECT * FROM user WHERE is_admin =1 ");
+			
+			ResultSet result = p.executeQuery();
+			while(result.next()) {
+				User user = User.getUser(result.getInt("user_id"));
+				results.add(user);
+			}
+			return results;
+		} catch (SQLException e) {
+			return null;
+		}
+	}
+	
+	// search for a user based on its name 
+	public static ArrayList<User> search(String name) {
+		try {
+			ArrayList<User> results = new ArrayList<User>();
+			PreparedStatement p = db.prepareStatement("SELECT * FROM user WHERE name LIKE ? ORDER BY user_id");
+			p.setString(1, "%" + name + "%");
+			
+			ResultSet result = p.executeQuery();
+			while(result.next()) {
+				User u = User.getUser(result.getInt("user_id"));
+				results.add(u);
+			}
+			return results;
+		} catch (SQLException e) {
+			return null;
+		}		
+	}
+	
+	public static void removeUser(User user) {
+		try {
+			db.prepareStatement("DELETE FROM user WHERE user_id = " + user.id).executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
